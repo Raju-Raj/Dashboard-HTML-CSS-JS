0cc6d96e349c3e91f4cdc5c77a851e45ad613e91
dev global <brajuraj1997@gmail.com>
2023-04-03 15:05:46 +0530

    Dashboard page

---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+  {
+    productName: "Foldable Mini Drone",
+    ProductNumber: "85631",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "LARVENDER KF102 Drone",
+    ProductNumber: "36378",
+    paymentStatus: "Refunded",
+    shipping: "Declined",
+  },
+  {
+    productName: "Drone with Camera Drone",
+    ProductNumber: "96996",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+  {
+    productName: "Ruko F11 Pro Drone",
+    ProductNumber: "49347",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "GPS 4k Drone",
+    ProductNumber: "22821",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+  {
+    productName: "DJI Air 25",
+    ProductNumber: "81475",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Lozenge Drone",
+    ProductNumber: "00786",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+];
